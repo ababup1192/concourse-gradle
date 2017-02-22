bbe9deb839762fbc590b5fb869a737c015fa3a53
abab <ababup1192@gmail.com>
2017-02-22 22:04:40 +0900

    Modify

--- commit-report.sh
+++ commit-report.sh
@@ -6,8 +6,8 @@ cd updated-report/
 
 cp -a ../out/build/reports ../out/build/test-results ./
 
-# git config --global user.email "ababup1192@gmail.com"
-# git config --global user.name "ababup1192"
+git config --global user.email "nobody@concourse.ci"
+git config --global user.name "Concourse"
 
 git remote -v
 
